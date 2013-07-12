2ab63d5aa918202e193ba30225079088e9aa228a
Albert Lee <Monsterblader@gmail.com>
2013-07-12 11:23:54 -0700

    Test express.

--- web.js
+++ web.js
@@ -3,27 +3,14 @@ var fs = require("fs");
 var http = require("http");
 var url = require("url");
 
-//var app = express();
-//app.use(express.logger());
-//
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-//
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var app = express();
+app.use(express.logger());
 
-var requestListener = function(req, res) {
-	if (req.method === "POST") {
-		res.writeHead(200, {'content-type': 'text/html'});
-		res.end("Moo, I say.");
-	} else {
-		res.writeHead(200, {'content-type': 'text/html'});
-		res.end("You say, 'moo.'");
-	}
-};
+app.get('/', function(request, response) {
+  response.send('<div>Hello World!</div>');
+});
 
-var server = http.createServer(requestListener);
-server.listen(process.env.PORT || 8080, process.env.IP || "127.0.0.1");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
