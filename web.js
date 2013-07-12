e994151f7d0c29290e30cb26b6139ce4a7e1d0a3
Albert Lee <Monsterblader@gmail.com>
2013-07-12 11:28:49 -0700

    Test express.

--- web.js
+++ web.js
@@ -6,8 +6,12 @@ var url = require("url");
 var app = express();
 app.use(express.logger());
 
+var cssString = fs.readFileSync("assets/style/style.css", "utf8", readFSCallback);
+var jsString = fs.readFileSync("assets/scripts/script.js", "utf8", readFSCallback);
+var webPage = fs.readFileSync("index.html", "utf8", readFSCallback);
+var preJS = webPage.replace("<!--style.css-->", cssString);
 app.get('/', function(request, response) {
-  response.send('<b>Hello World!</b>');
+	response.send(preJS.replace("\/\/script.js", jsString));
 });
 
 var port = process.env.PORT || 5000;
