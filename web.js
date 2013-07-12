7c851aaf24f9d6a7b777132f97dc1938306556a8
Albert Lee <Monsterblader@gmail.com>
2013-07-12 11:14:54 -0700

    Added server.listen.

--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var fs = require("fs");
 var http = require("http");
 var url = require("url");
 
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var app = express();
+//app.use(express.logger());
+//
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+//
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
 var requestListener = function(req, res) {
 	if (req.method === "POST") {
